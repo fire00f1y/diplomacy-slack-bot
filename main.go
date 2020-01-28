eb817256ba375a1719bad732a28936747f386d27
fire00f1y@gmail.com <fire00f1y@gmail.com>
2020-01-27 21:50:23 -0500

    test block body

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 	"net/http"
 )
 
+var block = `{
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*It's 80 degrees right now.*"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Partly cloudy today and tomorrow"
+            }
+        }
+    ]
+}`
+
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
@@ -22,5 +41,6 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!")
 		return
 	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, html.EscapeString(block))
 }
